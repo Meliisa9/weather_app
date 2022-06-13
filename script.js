b3d5abec1ce7a9d011c259e2281bcbced575eb2a
Meliisa9 <melisa_lolz@hotmail.com>
2022-06-13 17:30:03 +0200

    added math.round to temp

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let weather = {
         document.querySelector(".icon").src = 
             "https://openweathermap.org/img/wn/"+ icon + ".png"
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C"; 
+        document.querySelector(".temp").innerText = (Math.round(temp)) + "°C"; 
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
@@ -42,4 +42,4 @@ document.querySelector(".search-bar").addEventListener("keyup", function(event) 
     }
 });
 
-weather.fetchWeather("Denver");
+weather.fetchWeather("New York");
